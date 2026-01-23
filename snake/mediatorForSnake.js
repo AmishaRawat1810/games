ac1bce260abcb4e29e15c5aa2a33e691e35cae61
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2026-01-23 12:41:53 +0530

    refactor: Snake moves based on the keyStroke
    * Uses setRaw for moving the snake
    * Uses while loop instead of setInterval
    * Movement supports a,s,w,d and arrow up,down,right,left keys
    * Game ends if invalid key is pressed,snake touches itself or boundary

--- snake/mediatorForSnake.js
+++ snake/mediatorForSnake.js
@@ -1,5 +1,19 @@
-export const getInput = () => {
-    const move = Deno.readTextFileSync("./input.txt").at(-1);
-    console.log({ move });
-    return move;
+Deno.stdin.setRaw(true, { cbreak: true });
+
+const mapToKeyStroke = {
+  "279165": "w",
+  "279166": "s",
+  "279167": "d",
+  "279168": "a",
+  "9700": "a",
+  "11500": "s",
+  "10000": "d",
+  "11900": "w",
+};
+
+export const getInput = async () => {
+  const buf = new Uint8Array(3);
+  await Deno.stdin.read(buf);
+  const keyStroke = buf.join("");
+  return mapToKeyStroke[keyStroke];
 };
--- snake/snake_game.js
+++ snake/snake_game.js
@@ -4,22 +4,20 @@ import { displayScreen, drawOnDisplay } from "./board_functions.js";
 import { max, min, screen, snake } from "./global_snake_var.js";
 import { placeFrogs } from "./foodForSnake.js";
 
-const start = (snake, screen) => {
+const start = async (snake, screen) => {
   drawOnDisplay(snake, screen, snake.char);
-  let status = true;
+  displayScreen(screen);
+  let gameContinue = true;
 
-  const id = setInterval(() => {
-    placeFrogs(screen, min, max);
-    const move = getInput();
+  while (gameContinue) {
     console.clear();
+    placeFrogs(screen, min, max);
     displayScreen(screen);
-    if (!status || !"aswd".includes(move)) clearInterval(id);
-    status = executeMove(snake, screen, move);
+    const move = await getInput();
+    if (!"aswd".includes(move)) gameContinue = false;
+    gameContinue = executeMove(snake, screen, move);
     drawOnDisplay(snake, screen, "🟩");
-  }, 1000);
+  }
 };
 
-const id = setTimeout(() => {
-  start(snake, screen);
-  clearTimeout(id);
-}, 500);
+await start(snake, screen);
